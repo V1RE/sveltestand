5b3ecb908ae9aa7bf892a3ee9eb61e8db95f9edc
Niels Mentink <niels.mentink@gmail.com>
2021-06-12 18:38:46 +0200

    Added postcss nesting

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -7,6 +7,7 @@ const dev = mode === "development";
 
 const config = {
 	plugins: [
+    require("postcss-nested"),
 		// Some plugins, like postcss-nested, need to run before Tailwind
 		tailwindcss,
 		// But others, like autoprefixer, need to run after
--- svelte.config.js
+++ svelte.config.js
@@ -4,9 +4,15 @@ import preprocess from 'svelte-preprocess';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [preprocess({
-        "postcss": true
-    })],
+	preprocess: [
+		preprocess({
+			postcss: true,
+      defaults: {
+        script: 'typescript',
+        style: 'postcss'
+      }
+		})
+	],
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
